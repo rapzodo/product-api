18b50454f890a5f506cd37c68b53f1abce668adb
danilocastro <danilocastro81@gmail.com>
2021-01-21 16:12:28 -0600

    minor changes

--- src/main/java/com/target/product/api/controller/ProductController.java
+++ src/main/java/com/target/product/api/controller/ProductController.java
@@ -6,6 +6,7 @@ import com.target.product.api.models.PriceUpdateRequest;
 import com.target.product.api.services.DefaultProductService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -20,13 +21,13 @@ public class ProductController {
 
     @GetMapping("{id}")
     @ApiOperation(value = "get product details given the product id")
-    private ResponseEntity<Product> getProduct(@PathVariable String id) {
+    public ResponseEntity<Product> getProduct(@ApiParam(value = "the product id", example = "54456119") @PathVariable String id) {
         return ResponseEntity.ok(defaultProductService.getProduct(id));
     }
 
     @PutMapping("{id}")
     @ApiOperation(value = "Updates the current price for a given product Id")
-    public ResponseEntity<CurrentPrice> updateProductPrice(@PathVariable String id, @RequestBody PriceUpdateRequest priceUpdateRequest) {
+    public ResponseEntity<CurrentPrice> updateProductPrice(@ApiParam(value = "the product id", example = "54456119") @PathVariable String id, @RequestBody PriceUpdateRequest priceUpdateRequest) {
         return ResponseEntity.ok(defaultProductService.updateCurrentPrice(id, priceUpdateRequest));
     }
 }
